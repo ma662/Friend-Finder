d3667cff62bf1e6be74f958edf9f4fc6de81286c
ma662 <ma662@njit.edu>
2019-03-07 00:54:12 -0500

    did stuff

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,7 +3,7 @@ var path = require("path");
 module.exports = function(app) {
 
     app.get("/survey", function(req, res) {
-        return res.sendFile(path.join(__dirname, "../public/survey.html"));
+        return res.sendFile(path.join(__dirname, "../public/survey.html", ));
     });
     
     app.get("/", function(req, res) {
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // init dependencies
 var express = require("express");
-// var path = require("path");
 
 var app = express();
 var PORT = process.envPORT || 9001;
@@ -8,11 +7,10 @@ var PORT = process.envPORT || 9001;
 // init express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/app/public'));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
-
-// require("./routes/apiRoutes")(app);
 
 app.listen(PORT, () => {
     console.log("Listening on http://localhost:" + PORT);
